e30c01112cc8c45627a10a4c1bc9a81fb3ecba83
Stephane <Stephane@192.168.1.22>
2015-12-20 18:48:27 +0100

    hi

--- School/src/com/skula/school/models/Morse.java
+++ School/src/com/skula/school/models/Morse.java
@@ -10,42 +10,42 @@ public class Morse {
 
 	static {
 		CHARACTERS = new ArrayList<MorseChar>();
-		CHARACTERS.add(new MorseChar("A", new int[] { SHORT, LONG }));// 1
-		CHARACTERS.add(new MorseChar("B", new int[] { LONG, SHORT, SHORT, SHORT })); // 1, 2
-		CHARACTERS.add(new MorseChar("C", new int[] { LONG, SHORT, LONG, SHORT })); // 1, 4
-		CHARACTERS.add(new MorseChar("D", new int[] { 1, 0, 0, 1, 1, 0 })); // 1, 4,5
-		CHARACTERS.add(new MorseChar("E", new int[] { 1, 0, 0, 0, 1, 0 }));// 1,5}));
-		CHARACTERS.add(new MorseChar("F", new int[] { 1, 1, 0, 1, 0, 0 }));// 1,2,4}));
-		CHARACTERS.add(new MorseChar("G", new int[] { 1, 1, 0, 1, 1, 0 }));// 1,2,4,5}));
-		CHARACTERS.add(new MorseChar("H", new int[] { 1, 1, 0, 0, 1, 0 }));// 1,2,5}));
-		CHARACTERS.add(new MorseChar("I", new int[] { 0, 1, 0, 1, 0, 0 }));// 2,4}));
-		CHARACTERS.add(new MorseChar("J", new int[] { 0, 1, 0, 1, 1, 0 }));// 2,4,5}));
-		CHARACTERS.add(new MorseChar("K", new int[] { 1, 0, 1, 0, 0, 0 }));// 1,3}));
-		CHARACTERS.add(new MorseChar("L", new int[] { 1, 1, 1, 0, 0, 0 }));// 1,2,3}));
-		CHARACTERS.add(new MorseChar("M", new int[] { 1, 0, 1, 1, 0, 0 }));// 1,3,4}));
-		CHARACTERS.add(new MorseChar("N", new int[] { 1, 0, 1, 1, 1, 0 }));// 1,3,4,5}));
-		CHARACTERS.add(new MorseChar("O", new int[] { 1, 0, 1, 0, 1, 0 }));// 1,3,5}));
-		CHARACTERS.add(new MorseChar("P", new int[] { 1, 1, 1, 1, 0, 0 }));// 1,2,3,4}));
-		CHARACTERS.add(new MorseChar("Q", new int[] { 1, 1, 1, 1, 1, 0 }));// 1,2,3,4,5}));
-		CHARACTERS.add(new MorseChar("R", new int[] { 1, 1, 1, 0, 1, 0 }));// 1,2,3,5}));
-		CHARACTERS.add(new MorseChar("S", new int[] { 0, 1, 1, 1, 0, 0 }));// 2,3,4}));
-		CHARACTERS.add(new MorseChar("T", new int[] { 0, 1, 1, 1, 1, 0 }));// 2,3,4,5}));
-		CHARACTERS.add(new MorseChar("U", new int[] { 1, 0, 1, 0, 0, 1 }));// 1,3,6}));
-		CHARACTERS.add(new MorseChar("V", new int[] { 1, 1, 1, 0, 0, 1 }));// 1,2,3,6}));
-		CHARACTERS.add(new MorseChar("W", new int[] { 0, 1, 0, 1, 1, 1 }));// 2,4,5,6}));
-		CHARACTERS.add(new MorseChar("X", new int[] { 1, 0, 1, 1, 0, 1 }));// 1,3,4,6}));
-		CHARACTERS.add(new MorseChar("Y", new int[] { 1, 0, 1, 1, 1, 1 }));// 1,3,4,5,6}));
-		CHARACTERS.add(new MorseChar("Z", new int[] { 1, 0, 1, 0, 1, 1 }));// 1,3,5,6}));
+		CHARACTERS.add(new MorseChar("A", new int[] { SHORT, LONG }));
+		CHARACTERS.add(new MorseChar("B", new int[] { LONG, SHORT, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("C", new int[] { LONG, SHORT, LONG, SHORT }));
+		CHARACTERS.add(new MorseChar("D", new int[] { LONG, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("E", new int[] { SHORT }));
+		CHARACTERS.add(new MorseChar("F", new int[] { SHORT, SHORT, LONG, SHORT }));
+		CHARACTERS.add(new MorseChar("G", new int[] { LONG, LONG, SHORT }));
+		CHARACTERS.add(new MorseChar("H", new int[] { SHORT, SHORT, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("I", new int[] { SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("J", new int[] { SHORT, LONG, LONG, LONG }));
+		CHARACTERS.add(new MorseChar("K", new int[] { LONG, SHORT, LONG }));
+		CHARACTERS.add(new MorseChar("L", new int[] { SHORT, LONG, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("M", new int[] { LONG, LONG }));
+		CHARACTERS.add(new MorseChar("N", new int[] { LONG, SHORT }));
+		CHARACTERS.add(new MorseChar("O", new int[] { LONG, LONG, LONG }));
+		CHARACTERS.add(new MorseChar("P", new int[] { SHORT, LONG, LONG, SHORT }));
+		CHARACTERS.add(new MorseChar("Q", new int[] { LONG, LONG, SHORT, LONG }));
+		CHARACTERS.add(new MorseChar("R", new int[] { SHORT, LONG, SHORT }));
+		CHARACTERS.add(new MorseChar("S", new int[] { SHORT, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("T", new int[] { LONG }));
+		CHARACTERS.add(new MorseChar("U", new int[] { SHORT, SHORT, LONG }));
+		CHARACTERS.add(new MorseChar("V", new int[] { SHORT, SHORT, SHORT, LONG }));
+		CHARACTERS.add(new MorseChar("W", new int[] { SHORT, LONG, LONG }));
+		CHARACTERS.add(new MorseChar("X", new int[] { LONG, SHORT, SHORT, LONG }));
+		CHARACTERS.add(new MorseChar("Y", new int[] { LONG, SHORT, LONG, LONG }));
+		CHARACTERS.add(new MorseChar("Z", new int[] { LONG, LONG, SHORT, SHORT }));
 
-		CHARACTERS.add(new MorseChar("0", new int[] { 0, 1, 0, 1, 1, 0 }));// 2,4,5}));
-		CHARACTERS.add(new MorseChar("1", new int[] { 1, 0, 0, 0, 0, 0 }));// 1
-		CHARACTERS.add(new MorseChar("2", new int[] { 1, 1, 0, 0, 0, 0 })); // 1, 2
-		CHARACTERS.add(new MorseChar("3", new int[] { 1, 0, 0, 1, 0, 0 })); // 1, 4
-		CHARACTERS.add(new MorseChar("4", new int[] { 1, 0, 0, 1, 1, 0 })); // 1, 4,5
-		CHARACTERS.add(new MorseChar("5", new int[] { 1, 0, 0, 0, 1, 0 }));// 1,5}));
-		CHARACTERS.add(new MorseChar("6", new int[] { 1, 1, 0, 1, 0, 0 }));// 1,2,4}));
-		CHARACTERS.add(new MorseChar("7", new int[] { 1, 1, 0, 1, 1, 0 }));// 1,2,4,5}));
-		CHARACTERS.add(new MorseChar("8", new int[] { 1, 1, 0, 0, 1, 0 }));// 1,2,5}));
-		CHARACTERS.add(new MorseChar("9", new int[] { 0, 1, 0, 1, 0, 0 }));// 2,4}));
+		CHARACTERS.add(new MorseChar("0", new int[] { LONG, LONG, LONG, LONG, LONG }));
+		CHARACTERS.add(new MorseChar("1", new int[] { SHORT, LONG, LONG, LONG, LONG }));
+		CHARACTERS.add(new MorseChar("2", new int[] { SHORT, SHORT, LONG, LONG, LONG }));
+		CHARACTERS.add(new MorseChar("3", new int[] { SHORT, SHORT, SHORT, LONG, LONG }));
+		CHARACTERS.add(new MorseChar("4", new int[] { SHORT, SHORT, SHORT, SHORT, LONG }));
+		CHARACTERS.add(new MorseChar("5", new int[] { SHORT, SHORT, SHORT, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("6", new int[] { LONG, SHORT, SHORT, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("7", new int[] { LONG, LONG, SHORT, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("8", new int[] { LONG, LONG, LONG, SHORT, SHORT }));
+		CHARACTERS.add(new MorseChar("9", new int[] { LONG, LONG, LONG, LONG, SHORT }));
 	}
 }
